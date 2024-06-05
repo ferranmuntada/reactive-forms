30e585b5acd065742ff071eb10034df6af9804ae
Montix <fmuntadap@gmail.com>
2024-06-05 11:53:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      adress: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+      }),
+      phoneNumbers: this.fb.array([
+        this.fb.control('', [
+          Validators.required,
+          Validators.pattern(/^\d{10}$/),
+        ]),
+      ]),
+    });
+  }
+
+  get phoneNumbers() {
+    return this.userForm.get('phoneNumbers') as FormArray;
+  }
+
+  addPhoneNumber() {
+    this.phoneNumbers.push(
+      this.fb.control('', [Validators.required, Validators.pattern(/^\d{10}$/)])
+    );
+  }
+
+  removePhoneNumber(index: number) {
+    this.phoneNumbers.removeAt(index);
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
+
+  isInvalid(controlName: string, nestedControlName?: string): boolean {
+    const control = nestedControlName
+      ? this.userForm.get([controlName, nestedControlName])
+      : this.userForm.get(controlName);
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
+  }
+
+  isInvalidPhone(index: number): boolean {
+    const control = this.phoneNumbers.at(index);
+    return control.invalid && (control.dirty || control.touched);
+  }
 }
